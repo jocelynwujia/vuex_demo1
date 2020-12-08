7c92a571986468470024c710f4c1ec3102bc0b0c
jocelyn <499264478@qq.com>
2020-12-08 18:39:45 +0800

    初始化计数器项目的基本结构

---
+++ src/components/Addition.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>当前最新从count值为：</h3>
+        <button>+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
